e9e9f64782146d9e047433e995eae7bddcb717b9
Robot-LT <hassan.doodiyan@gmail.com>
2025-05-29 11:45:53 +0330

    Imolement Models But Not Define Them

---
+++ src/model/FullList.ts
@@ -0,0 +1,23 @@
+import ListItem from "./ListItem";
+
+interface List {
+    list: ListItem[],
+    load(): void,
+    save(): void,
+    clear(): void,
+    add(item: ListItem): void,
+    remove(id: string): void,
+    setChecked(id: string): void,
+}
+
+export default class FullList implements List{
+    
+    constructor(){}
+    public get list():ListItem[] {
+        return this.list
+    }
+    public set list(list: ListItem[]){
+        this.list = list
+    }
+
+}
